c95043f062676cf98fd790bf142a38133f8d1ec1
Dennis <b976543257@gmail.com>
2020-08-16 00:13:38 +0800

    Add bootstrap js & Update Sidebar options

--- src/components/common/Sidebar.vue
+++ src/components/common/Sidebar.vue
@@ -5,81 +5,38 @@
       class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse"
     >
       <div class="sidebar-sticky pt-3">
-        <ul class="nav flex-column">
-          <li class="nav-item">
-            <a class="nav-link active" href="#">
-              <span data-feather="home"></span>
-              Dashboard <span class="sr-only">(current)</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="file"></span>
-              Orders
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="shopping-cart"></span>
-              Products
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="users"></span>
-              Customers
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="bar-chart-2"></span>
-              Reports
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="layers"></span>
-              Integrations
-            </a>
-          </li>
-        </ul>
-
         <h6
           class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted"
         >
-          <span>Saved reports</span>
-          <a
-            class="d-flex align-items-center text-muted"
-            href="#"
-            aria-label="Add a new report"
-          >
-            <span data-feather="plus-circle"></span>
-          </a>
+          123
         </h6>
-        <ul class="nav flex-column mb-2">
+        <ul class="nav flex-column">
           <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="file-text"></span>
-              Current month
+            <a class="nav-link active" href="#">
+              <span>產品列標</span>
             </a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">
-              <span data-feather="file-text"></span>
-              Last quarter
+              <span>訂單列表</span>
             </a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">
-              <span data-feather="file-text"></span>
-              Social engagement
+              <span>優惠券</span>
             </a>
           </li>
+        </ul>
+        <h6
+          class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted"
+        >
+          123
+        </h6>
+        <ul class="nav flex-column">
           <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="file-text"></span>
-              Year-end sale
-            </a>
+            <router-link class="nav-link" to="/order">
+              <span>模擬訂單</span>
+            </router-link>
           </li>
         </ul>
       </div>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './package/router';
+import './package/bootstrap';
 import './package/axios';
 
 Vue.config.productionTip = false;
