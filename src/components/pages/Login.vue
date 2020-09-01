c196f2c70a1cbc360afc5f647fc1c07fef078e9e
蔡名彥 <a976543257@gmail.com>
2020-09-01 14:03:26 +0800

    update login.vue

--- src/components/pages/Login.vue
+++ src/components/pages/Login.vue
@@ -1,13 +1,13 @@
 <template>
-  <div>
+  <div class="row align-items-center mt">
     <form class="form-signin" @submit.prevent="login">
-      <h1 class="h3 mb-3 font-weight-normal">登入</h1>
+      <h1 class="h3 mb-3 font-weight-normal text-center">登入後台</h1>
       <label for="inputEmail" class="sr-only">Email address</label>
       <input
         type="email"
         id="inputEmail"
         class="form-control"
-        placeholder="Email address"
+        placeholder="請輸入信箱"
         v-model="user.username"
         required
         autofocus
@@ -17,19 +17,19 @@
         type="password"
         id="inputPassword"
         class="form-control"
+        placeholder="請輸入密碼"
         v-model="user.password"
-        placeholder="Password"
         required
       />
-      <div class="checkbox mb-3">
-        <label>
-          <input type="checkbox" value="remember-me" /> Remember me
-        </label>
-      </div>
-      <button class="btn btn-lg btn-primary btn-block" type="submit">
-        Sign in
+      <button class="btn btn-lg btn-primary btn-block " type="submit">
+        <span v-if="canLogin === false">登入</span>
+        <font-awesome-icon
+          v-else-if="canLogin === true"
+          icon="spinner"
+          class="fa-spin"
+        />
       </button>
-      <p class="mt-5 mb-3 text-muted">&copy; 2017-2020</p>
+      <span class="text-danger">{{ error }}</span>
     </form>
   </div>
 </template>
@@ -41,17 +41,24 @@ export default {
       user: {
         username: '',
         password: ''
-      }
+      },
+      error: '',
+      canLogin: false
     };
   },
   methods: {
     async login() {
+      this.canLogin = true;
       const api = `${process.env.VUE_APP_URL}/admin/signin`;
       const vm = this;
       const { data } = await this.$http.post(api, vm.user);
 
-      data.success ? this.$router.push('/products') : this.$router.go(0);
-      console.log(data);
+      if (data.success) {
+        this.$router.push('/products');
+      } else {
+        this.error = '帳號或密碼錯誤';
+      }
+      this.canLogin = false;
     }
   },
   components: {}
@@ -87,5 +94,8 @@ export default {
   margin-bottom: 10px;
   border-top-left-radius: 0;
   border-top-right-radius: 0;
+}
+.mt {
+  margin-top: 180px;
 }
 </style>
